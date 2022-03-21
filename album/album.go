861b970e34cf48bcf8dc1d21caef3dad8969fd7f
PriyankaGour <priyanka.g@canopas.com>
2022-03-21 15:06:08 +0530

    Added CRUD Operation over Album

--- album/album.go
+++ album/album.go
@@ -7,6 +7,7 @@ import (
 	// "net/http"
 	// "time"
 
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,7 @@ import (
 )
 
 type Album struct {
+	Id          int    `json:"album_id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	ImageUrl    string `json:"image_url"`
@@ -50,4 +52,74 @@ func (repository *AlbumRepository) Create(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"Message": "Album Created Successfully"})
+}
+
+// Select ARTIST
+func (repository *AlbumRepository) Read(c *gin.Context) {
+
+	input, err := repository.GetAlbum()
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, input)
+
+}
+
+func (repository *AlbumRepository) GetAlbum() (input []Album, err error) {
+
+	err = repository.Db.Select(&input, `SELECT name,description,image_url,is_published,artist_id from Album`)
+	if err != nil {
+		fmt.Println("error on display")
+		return
+
+	}
+
+	return
+}
+
+//UPDATE artist
+func (repository *AlbumRepository) Update(c *gin.Context) {
+	input := Album{}
+
+	err := c.ShouldBindWith(&input, binding.JSON)
+
+	if err != nil {
+		c.Abort()
+		c.JSON(http.StatusBadRequest, gin.H{"Message cannot bind the STRUCT ": err.Error()})
+		return
+	}
+
+	_, err = repository.Db.Exec(`UPDATE Album SET name=?,description=?,image_url=? ,is_published=? WHERE id=?`, input.Name, input.Description, input.Image_url, input.IsPublished, input.Id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message cannot Update ": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"Message": "Album Updated Successfully"})
+}
+
+// DELETE ARTIST
+func (repository *AlbumRepository) Delete(c *gin.Context) {
+	input := Album{}
+
+	err := c.ShouldBindWith(&input, binding.JSON)
+
+	if err != nil {
+		c.Abort()
+		c.JSON(http.StatusBadRequest, gin.H{"Message cannot bind the STRUCT ": err.Error()})
+		return
+	}
+
+	_, err = repository.Db.Exec(`DELETE From Album   WHERE id=?`, input.Id)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message cannot DELETE ": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"Message": "Album  Removed Successfully"})
 }
