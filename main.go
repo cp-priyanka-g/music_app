8d320d7169862b79da09845087850b53ed3983a2
PriyankaGour <priyanka.g@canopas.com>
2022-04-08 11:38:55 +0530

    Add Refresh Token at the time of User Register

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"album"
-	"artist"
 	"db"
-	"favourite"
-	"fmt"
-	"net/http"
-	"playlist"
 	"register"
 
-	"track"
-
-	"github.com/dgrijalva/jwt-go"
 	"github.com/jmoiron/sqlx"
 
 	"github.com/gin-gonic/gin"
@@ -28,89 +19,89 @@ func main() {
 	_ = r.Run(":8080")
 }
 
-func AuthorizeJWT() gin.HandlerFunc {
-	return func(c *gin.Context) {
+// func AuthorizeJWT() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
 
-		authHeader := c.GetHeader("Authorization")
-		tokenString := authHeader
-		token, err := register.JWTAuthService().ValidateToken(tokenString)
-		if token.Valid {
-			claims := token.Claims.(jwt.MapClaims)
-			fmt.Println(claims)
-		} else {
-			fmt.Println(err)
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
+// 		authHeader := c.GetHeader("Authorization")
+// 		tokenString := authHeader
+// 		token, err := register.JWTAuthService().ValidateToken(tokenString)
+// 		if token.Valid {
+// 			claims := token.Claims.(jwt.MapClaims)
+// 			fmt.Println(claims)
+// 		} else {
+// 			fmt.Println(err)
+// 			c.AbortWithStatus(http.StatusUnauthorized)
+// 		}
 
-	}
-}
+// 	}
+// }
 func setupRouter(sqlDb *sqlx.DB) *gin.Engine {
 	router := gin.Default()
 
-	var loginService register.LoginService = register.StaticLoginService()
-	var jwtService register.JWTService = register.JWTAuthService()
-	var loginController register.LoginController = register.LoginHandler(loginService, jwtService)
+	// var loginService register.LoginService = register.StaticLoginService()
+	// var jwtService register.JWTService = register.JWTAuthService()
+	// var loginController register.LoginController = register.LoginHandler(loginService, jwtService)
 
-	router.POST("/api/login", func(ctx *gin.Context) {
-		token := loginController.Login(ctx)
-		if token != "" {
-			ctx.JSON(http.StatusOK, gin.H{
-				"token": token,
-			})
-		} else {
-			ctx.JSON(http.StatusUnauthorized, nil)
-		}
-	})
+	// router.POST("/api/login", func(ctx *gin.Context) {
+	// 	token := loginController.Login(ctx)
+	// 	if token != "" {
+	// 		ctx.JSON(http.StatusOK, gin.H{
+	// 			"token": token,
+	// 		})
+	// 	} else {
+	// 		ctx.JSON(http.StatusUnauthorized, nil)
+	// 	}
+	// })
 
 	registerRepo := register.New(sqlDb)
-	artistRepo := artist.New(sqlDb)
-	albumRepo := album.New(sqlDb)
-	trackRepo := track.New(sqlDb)
-	playlistRepo := playlist.New(sqlDb)
-	favRepo := favourite.New(sqlDb)
+	// artistRepo := artist.New(sqlDb)
+	// albumRepo := album.New(sqlDb)
+	// trackRepo := track.New(sqlDb)
+	// playlistRepo := playlist.New(sqlDb)
+	// favRepo := favourite.New(sqlDb)
 
 	//USER Authencation
 	router.POST("/api/v1/register", registerRepo.Register)
 	router.POST("/api/v1/register/admin-register", registerRepo.RegisterAdmin)
 
 	//Adminauthorized wrapper class
-	apiRoutes := router.Group("/api", AuthorizeJWT())
-	{
-		apiRoutes.POST("/v1/artist", artistRepo.Create)
-		apiRoutes.PUT("/v1/artist/:id", artistRepo.Update)
-		apiRoutes.GET("/v1/artist/show", artistRepo.Read)
-		apiRoutes.DELETE("/v1/artist/remove", artistRepo.Delete)
+	// apiRoutes := router.Group("/api", AuthorizeJWT())
+	// {
+	// 	apiRoutes.POST("/v1/artist", artistRepo.Create)
+	// 	apiRoutes.PUT("/v1/artist/:id", artistRepo.Update)
+	// 	apiRoutes.GET("/v1/artist/show", artistRepo.Read)
+	// 	apiRoutes.DELETE("/v1/artist/remove", artistRepo.Delete)
 
-		//ALBUM
-		apiRoutes.POST("/v1/album", albumRepo.Create)
-		apiRoutes.PUT("/v1/album/edit", albumRepo.Update)
-		apiRoutes.DELETE("/v1/album/remove", albumRepo.Delete)
-		apiRoutes.GET("/v1/album/show", albumRepo.Read)
-		apiRoutes.POST("/v1/album/add", albumRepo.AddAlbum)
-		apiRoutes.DELETE("/api/v1/album/remove-track", albumRepo.RemoveAlbum)
+	// 	//ALBUM
+	// 	apiRoutes.POST("/v1/album", albumRepo.Create)
+	// 	apiRoutes.PUT("/v1/album/edit", albumRepo.Update)
+	// 	apiRoutes.DELETE("/v1/album/remove", albumRepo.Delete)
+	// 	apiRoutes.GET("/v1/album/show", albumRepo.Read)
+	// 	apiRoutes.POST("/v1/album/add", albumRepo.AddAlbum)
+	// 	apiRoutes.DELETE("/api/v1/album/remove-track", albumRepo.RemoveAlbum)
 
-		//Track
-		apiRoutes.POST("/v1/track", trackRepo.Create)
-		apiRoutes.PUT("/v1/track/edit", trackRepo.Update)
-		apiRoutes.DELETE("/v1/track/remove", trackRepo.Delete)
-		apiRoutes.GET("/v1/track/show", trackRepo.Read)
+	// 	//Track
+	// 	apiRoutes.POST("/v1/track", trackRepo.Create)
+	// 	apiRoutes.PUT("/v1/track/edit", trackRepo.Update)
+	// 	apiRoutes.DELETE("/v1/track/remove", trackRepo.Delete)
+	// 	apiRoutes.GET("/v1/track/show", trackRepo.Read)
 
-		//Playlist
-		apiRoutes.POST("/v1/playlist", playlistRepo.Create)
-		apiRoutes.PUT("/v1/playlist/edit", playlistRepo.Update)
-		apiRoutes.DELETE("/v1/playlist/remove", playlistRepo.Delete)
-		apiRoutes.GET("/v1/playlist/show", playlistRepo.Read)
-		apiRoutes.POST("/v1/playlist/add-track-playlist", playlistRepo.AddPlaylist)
-		apiRoutes.DELETE("/v1/playlist/remove-track-playlist", playlistRepo.Remove)
-		apiRoutes.GET("/v1/playlist/get", playlistRepo.Get)
-	}
+	// 	//Playlist
+	// 	apiRoutes.POST("/v1/playlist", playlistRepo.Create)
+	// 	apiRoutes.PUT("/v1/playlist/edit", playlistRepo.Update)
+	// 	apiRoutes.DELETE("/v1/playlist/remove", playlistRepo.Delete)
+	// 	apiRoutes.GET("/v1/playlist/show", playlistRepo.Read)
+	// 	apiRoutes.POST("/v1/playlist/add-track-playlist", playlistRepo.AddPlaylist)
+	// 	apiRoutes.DELETE("/v1/playlist/remove-track-playlist", playlistRepo.Remove)
+	// 	apiRoutes.GET("/v1/playlist/get", playlistRepo.Get)
+	// }
 
 	// Favourite Track (User functionality API)
 
-	router.POST("/api/v1/favourite-track/create", favRepo.Create)
-	router.DELETE("/api/v1/unfavourite-track", favRepo.Delete)
-	router.GET("/api/v1/favourite-track", favRepo.Read)
-	router.GET("/api/v1/favourite-track/:id", favRepo.FavTrackId)
+	// router.POST("/api/v1/favourite-track/create", favRepo.Create)
+	// router.DELETE("/api/v1/unfavourite-track", favRepo.Delete)
+	// router.GET("/api/v1/favourite-track", favRepo.Read)
+	// router.GET("/api/v1/favourite-track/:id", favRepo.FavTrackId)
 
 	//Test ENDPOINTS
 	router.GET("/api/ping", func(c *gin.Context) {
--- register/register.go
+++ register/register.go
@@ -1,9 +1,8 @@
 package register
 
 import (
-	"fmt"
 	"net/http"
-	"os"
+
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -23,68 +22,29 @@ type RegisterRepository struct {
 	Db *sqlx.DB
 }
 
-//jwt service
-type JWTService interface {
-	GenerateToken(email string, isUser bool) string
-	ValidateToken(token string) (*jwt.Token, error)
+type Authentication struct {
+	Email string `json:"email"`
 }
 
-type authCustomClaims struct {
-	Name string `json:"name"`
-	User bool   `json:"user"`
-	jwt.StandardClaims
+type Token struct {
+	Role        string `json:"role"`
+	Email       string `json:"email"`
+	TokenString string `json:"token"`
 }
 
-type jwtServices struct {
-	secretKey string
-	issure    string
-}
+func generateRefreshToken() string {
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	rtClaims := refreshToken.Claims.(jwt.MapClaims)
+	rtClaims["sub"] = 1
+	rtClaims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 
-//auth-jwt
-func JWTAuthService() JWTService {
-	return &jwtServices{
-		secretKey: getSecretKey(),
-		issure:    "Priya",
-	}
-}
+	rt, err := refreshToken.SignedString([]byte("secret"))
 
-func getSecretKey() string {
-	secret := os.Getenv("SECRET")
-	if secret == "" {
-		secret = "secret"
-	}
-	return secret
-}
-
-func (register *jwtServices) GenerateToken(email string, isUser bool) string {
-	claims := &authCustomClaims{
-		email,
-		isUser,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
-			Issuer:    register.issure,
-			IssuedAt:  time.Now().Unix(),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	//encoded string
-	t, err := token.SignedString([]byte(register.secretKey))
 	if err != nil {
 		panic(err)
 	}
-	return t
-}
 
-func (register *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, error) {
-	return jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
-		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
-			return nil, fmt.Errorf("Invalid token", token.Header["alg"])
-
-		}
-		return []byte(register.secretKey), nil
-	})
-
+	return rt
 }
 
 func New(db *sqlx.DB) *RegisterRepository {
@@ -95,20 +55,32 @@ func (repository *RegisterRepository) Register(c *gin.Context) {
 
 	input := UserRegister{}
 	err := c.ShouldBindWith(&input, binding.JSON)
+	var email string
 
 	if err != nil {
 		c.Abort()
-		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"Message ": err.Error()})
 		return
 	}
-	auth := JWTAuthService().GenerateToken(input.Email, true)
-	_, err = repository.Db.Exec(`INSERT INTO Users(name,email,user_type,auth_token) VALUES (?,?,?,?)`, input.Name, input.Email, "General", auth)
 
-	if err != nil {
-		panic(err)
+	err = repository.Db.Get(&email, `SELECT email from Users where email=?`, input.Email)
+
+	if email != input.Email {
+
+		auth := generateRefreshToken()
+
+		_, err = repository.Db.Exec(`INSERT INTO Users(name,email,user_type,auth_token) VALUES (?,?,?,?)`, input.Name, input.Email, "General", auth)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Register Successfully"})
+		c.JSON(http.StatusOK, auth)
+
+	} else {
+		c.JSON(403, gin.H{"message": "Email Already exist"})
 	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Register Successfully"})
 
 }
 
@@ -116,8 +88,7 @@ func (repository *RegisterRepository) Register(c *gin.Context) {
 func (repository *RegisterRepository) RegisterAdmin(c *gin.Context) {
 
 	input := UserRegister{}
-
-	auth := JWTAuthService().GenerateToken(input.Email, true)
+	var email string
 
 	err := c.ShouldBindWith(&input, binding.JSON)
 
@@ -127,63 +98,18 @@ func (repository *RegisterRepository) RegisterAdmin(c *gin.Context) {
 		return
 	}
 
-	_, err = repository.Db.Exec(`INSERT INTO Users(name,email,user_type,auth_token) VALUES (?,?,?,?)`, input.Name, input.Email, "Admin", auth)
+	err = repository.Db.Get(&email, `SELECT email from Users where email= ?`, input.Email)
 
-	if err != nil {
-		panic(err)
+	if email != input.Email {
+
+		auth := generateRefreshToken()
+
+		_, err = repository.Db.Exec(`INSERT INTO Users(name,email,user_type,auth_token) VALUES (?,?,?,?)`, input.Name, input.Email, "Admin", auth)
+
+		c.JSON(http.StatusOK, gin.H{"message": "Admin Registered Successfully"})
+		c.JSON(http.StatusOK, auth)
+
+	} else {
+		c.JSON(403, gin.H{"message": "Email Already exist"})
 	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Admin Registered Successfully"})
-
-}
-
-// LOGIN AUTHENTICATION
-type LoginService interface {
-	LoginUser(email string) bool
-}
-type loginInformation struct {
-	email string
-}
-
-func (info *loginInformation) LoginUser(email string) bool {
-	return info.email == email
-}
-
-func StaticLoginService() LoginService {
-	return &loginInformation{
-		email: "priyanka@gmail.com",
-	}
-}
-
-//login contorller interface
-
-type LoginController interface {
-	Login(ctx *gin.Context) string
-}
-
-type loginController struct {
-	loginService LoginService
-	jWtService   JWTService
-}
-
-func LoginHandler(loginService LoginService,
-	jWtService JWTService) LoginController {
-	return &loginController{
-		loginService: loginService,
-		jWtService:   jWtService,
-	}
-}
-
-func (register *loginController) Login(ctx *gin.Context) string {
-	var credential UserRegister
-	err := ctx.ShouldBind(&credential)
-	if err != nil {
-		return "no data found"
-	}
-	isUserAuthenticated := register.loginService.LoginUser(credential.Email)
-	if isUserAuthenticated {
-		return register.jWtService.GenerateToken(credential.Email, true)
-	}
-	return ""
-
 }
