143a9d540d97a419e34879b40bb9b119fb70bdde
PriyankaGour <priyanka.g@canopas.com>
2022-04-12 09:02:10 +0530

    Removed User middleware

--- main.go
+++ main.go
@@ -50,44 +50,12 @@ func AuthorizeJWT() gin.HandlerFunc {
 			if claims["role"] == "Admin" {
 				c.Request.Header.Set("Role", "Admin")
 				return
-
 			} else if claims["role"] == "General" {
 				c.Request.Header.Set("Role", "General")
 				return
 
 			}
-		}
-		fmt.Println(err)
-		c.AbortWithStatus(http.StatusUnauthorized)
 
-	}
-}
-
-//Middleware for user
-func UserAuthorizeJWT() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		authHeader := c.GetHeader("Token")
-		tokenString := authHeader
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Invalid token", token.Header["Token"])
-
-			}
-			return "secretKey", nil
-		})
-
-		if err != nil {
-			c.JSON(403, gin.H{"message": "Your Token has been expired."})
-			return
-		}
-
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			if claims["role"] == "General" {
-				c.Request.Header.Set("Role", "General")
-				return
-
-			}
 		}
 		fmt.Println(err)
 		c.AbortWithStatus(http.StatusUnauthorized)
@@ -139,21 +107,18 @@ func setupRouter(sqlDb *sqlx.DB) *gin.Engine {
 		authorized.POST("/v1/playlist/add-track-playlist", playlistRepo.AddPlaylist)
 		authorized.DELETE("/v1/playlist/remove-track-playlist", playlistRepo.Remove)
 
-	}
+		//Favourite Track (User functionality API)
 
-	//Favourite Track (User functionality API)
+		authorized.GET("/v1/album/show", albumRepo.Read)
+		authorized.GET("/v1/track/show", trackRepo.Read)
+		authorized.GET("/v1/playlist/show", playlistRepo.Read)
+		authorized.GET("/v1/playlist/get-playlist-track/:id", playlistRepo.PlaylistById)
 
-	authorizedUser := router.Group("/api", AuthorizeJWT())
-	{
-		authorizedUser.GET("/v1/album/show", albumRepo.Read)
-		authorizedUser.GET("/v1/track/show", trackRepo.Read)
-		authorizedUser.GET("/v1/playlist/show", playlistRepo.Read)
-		authorizedUser.GET("/v1/playlist/get-playlist-track/:id", playlistRepo.PlaylistById)
+		authorized.POST("/v1/favourite-track/create", favRepo.Create)
+		authorized.DELETE("/v1/unfavourite-track", favRepo.Delete)
+		authorized.GET("/v1/favourite-track", favRepo.Read)
+		authorized.GET("/v1/favourite-track/:id", favRepo.FavTrackId)
 
-		authorizedUser.POST("/v1/favourite-track/create", favRepo.Create)
-		authorizedUser.DELETE("/v1/unfavourite-track", favRepo.Delete)
-		authorizedUser.GET("/v1/favourite-track", favRepo.Read)
-		authorizedUser.GET("/v1/favourite-track/:id", favRepo.FavTrackId)
 	}
 
 	//Test ENDPOINTS
