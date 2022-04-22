2e2a41d31abdc2da743ef1c2e097cb4b62003887
PriyankaGour <priyanka.g@canopas.com>
2022-04-22 09:17:25 +0530

    Refactor code of version checking

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -13,41 +14,40 @@ import (
 func WelcomeEndpoint(c *gin.Context) {
 	c.Next()
 	dt := time.Now()
-	log.Printf("Current date and time is: ", dt.String())
+	fmt.Println("Current date and time is: ", dt.String())
 	fmt.Println("Formated date and time", dt.Format("01-02-2006 15:04:05"))
 	log.Printf("Endpoint URL is %v", c.Request.URL)
 
 }
 func VersionCheck(c *gin.Context) {
 
-	if c.FullPath() == "/v1/welcome" {
-		log.Printf("welcome to older version")
+	url := c.Request.URL.String()
+	fmt.Println("url:", url)
+	if strings.Contains(url, "/v1") {
+		fmt.Println("This is old version V1")
 	}
-
-	// url := c.Request.URL.String()
-	// for _, p := range c.Params {
-	// 	url = strings.Replace(url, p.Value, ":"+p.Key, 1)
-	// 	log.Printf("welcome to older version", url)
-	// }
-	// c.Set("matched_path", url)
 
 }
 
 func main() {
 	router := gin.Default()
-	router.Use(VersionCheck)
+	authorized := router.Group("/api", WelcomeEndpoint)
+	{
+		authorized.Use(VersionCheck)
 
-	router.GET("/v1/welcome", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to Middleware version v1",
+		authorized.GET("/v1/welcome", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Welcome to Middleware version v1",
+			})
 		})
-	})
 
-	router.GET("/v2/hello-world", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World version 2",
+		authorized.GET("/v2/hello-world", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Hello World version 2",
+			})
 		})
-	})
+
+	}
 
 	//Test ENDPOINTS
 	router.GET("/api/ping", func(c *gin.Context) {
